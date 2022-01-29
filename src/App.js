a8879a250473f5c04f7a3392cce5c0da53890edb
Italo Canturil <italo.cantur@gmail.com>
2022-01-28 22:56:44 -0300

    Add solidity event

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import { ethers } from "ethers";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import myEpicNFT from './utils/MyEpicNFT.json';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_PERSON = 'ItaloCantur';
+const TWITTER_HANDLE = 'ItaloCantur';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const TWITTER_SECOND = `https://twitter.com/${TWITTER_PERSON}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+let MINT_COUNT = 0;
+
+const CONTRACT_ADDRESS = "0xb580C3B11d499cc92Ca578Eeba91A73495A5666C";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
-  
     if (!ethereum) {
       console.log("Make sure you have metamask");
       return;
@@ -25,11 +26,20 @@ const App = () => {
     }
 
     const accounts = await ethereum.request({ method: 'eth_accounts'});
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+    const rinkebyChainId = "0x4"; 
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
 
     if(accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -48,14 +58,38 @@ const App = () => {
       
       console.log("connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      setupEventListener()
     } catch (error) {
       console.log(error);
     }
   }
 
-  const askContractToMintNFT = async () => {
-    const CONTRACT_ADDRESS = "0x69199026D2d1f98FAEcf9449B2cba15225DcE5c4";
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window
 
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNFT.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          MINT_COUNT = tokenId.toNumber();
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener")
+      } else {
+        console.log("Ethereum object doesn't exist")
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNFT = async () => {
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -105,6 +139,7 @@ const App = () => {
               Mint NFT
             </button>
           )}
+          <p>{`${MINT_COUNT}/${TOTAL_MINT_COUNT}`}</p>
         </div>
         <footer className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -113,13 +148,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-          <a
-            className="footer-text twitter-person"
-            href={TWITTER_SECOND}
-            target="_blank"
-            rel="noreferrer"
-          >{`by @${TWITTER_PERSON}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </footer>
       </div>
     </div>
